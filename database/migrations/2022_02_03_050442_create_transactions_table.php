40752fd51824a92c222ebfb6d42a6e2f952caf5a
sanryuu03 <ryuuosku@gmail.com>
2022-02-03 12:10:22 +0700

    migration and ERD

---
+++ database/migrations/2022_02_03_050442_create_transactions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('userID');
+            $table->integer('foodID');
+            $table->integer('quantity');
+            $table->integer('total');
+            $table->string('status');
+            $table->text('paymentUrl');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
