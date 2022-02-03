0f054c3279e82bd78778285b117d60922401254e
sanryuu03 <ryuuosku@gmail.com>
2022-02-03 12:34:12 +0700

    food models

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -2,10 +2,42 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Storage;
 
 class Food extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name', 'description', 'ingredients',
+        'price', 'rate', 'types', 'picturePath'
+    ];
+
+        // epoch UNIX time
+        public function getCreatedAtAttribute($value)
+        {
+            return Carbon::parse($value)->timestamp;
+        }
+
+        public function getUpdatedAtAttribute($value)
+        {
+            return Carbon::parse($value)->timestamp;
+        }
+
+        public function toArray()
+        {
+            // agar data picturePath di konversi jadi picture_path
+            $toArray = parent::toArray();
+            $toArray['picturePath'] = $this->picturePath;
+            return $toArray;
+        }
+
+        public function getPicturePathAttribute()
+        {
+            return url('') . Storage::url($this->attributes['picturePath']);
+        }
 }
