fc04b79328a5d628eb1091cccdbe8eae9f74ff25
sanryuu03 <ryuuosku@gmail.com>
2022-02-03 13:08:23 +0700

    transaction models

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,10 +2,39 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Transaction extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'userID', 'foodID', 'quantity',
+        'total', 'status', 'paymentUrl'
+    ];
+
+    // relasi
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id', 'userID');
+    }
+
+    public function food()
+    {
+        return $this->hasOne(Food::class, 'id', 'foodID');
+    }
+
+    // epoch UNIX time
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->timestamp;
+    }
+
+    public function getUpdatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->timestamp;
+    }
 }
