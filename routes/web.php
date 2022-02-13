d0740aea583863ae628f9b9af24dadce20fead68
sanryuu03 <ryuuosku@gmail.com>
2022-02-13 15:49:08 +0700

    crud food

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\MidtransController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FoodController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -34,6 +35,7 @@ Route::get('/', function () {
 Route::prefix('dashboard')->middleware(['auth:sanctum','admin'])->group(function() {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('users', UserController::class);
+    Route::resource('food', FoodController::class);
 });
 
 // midtrans related
