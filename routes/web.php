d91524c66edd2ecf4d4ce059dcc7f7d6b0511265
sanryuu03 <ryuuosku@gmail.com>
2022-02-13 21:11:52 +0700

    crud transactions

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\API\MidtransController;
-use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FoodController;
 use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\API\MidtransController;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,6 +37,8 @@ Route::prefix('dashboard')->middleware(['auth:sanctum','admin'])->group(function
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('users', UserController::class);
     Route::resource('food', FoodController::class);
+    Route::get('transactions/{id}/status/{status}', [TransactionController::class, 'changeStatus'])->name('transactions.changeStatus');
+    Route::resource('transactions', TransactionController::class);
 });
 
 // midtrans related
